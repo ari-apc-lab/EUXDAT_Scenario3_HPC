b401e0baa7f46660bdf76480e1f760eccd434c6e
euxdat <none@the.moment>
2020-04-21 15:20:16 +0000

    adapted parallel version of script for execution on HLRS Hawk with jobscript- PM
    Signed-off-by: euxdat <none@the.moment>

--- frostdates/cloudify_integration/python_code/frostdates_params.py
+++ frostdates/cloudify_integration/python_code/frostdates_params.py
@@ -1,27 +1,28 @@
 import os
 import logging
 
-DAY_IN_ROW = os.getenv('DAY_IN_ROW')
-START_HOUR_DAY = os.getenv('START_HOUR_DAY')
-END_HOUR_DAY = os.getenv('END_HOUR_DAY')
-FROST_DEGREE = os.getenv('FROST_DEGREE')
-START_YEAR = os.getenv('START_YEAR')
-END_YEAR = os.getenv('END_YEAR')
-PROBABILITY = os.getenv('PROBABILITY')
+DAY_IN_ROW = 1 
+START_HOUR_DAY = 0 
+END_HOUR_DAY = 23
+FROST_DEGREE = 0
+START_YEAR = 2016
+END_YEAR = 2018
+PROBABILITY = 10
 
-EXPORT_FOLDER = os.getenv('EXPORT_FOLDER')
-DATA_FOLDER = os.getenv('DATA_FOLDER')
+EXPORT_FOLDER = "/zhome/academic/HLRS/eur/xeupmara/sc3_agroclimatic/frostdates/cloudify_integration/export" 
+DATA_FOLDER = "/zhome/academic/HLRS/eur/xeupmara/sc3_agroclimatic/frostdates/cloudify_integration/data" 
 
-START_LON = os.getenv('START_LON')
-START_LAT = os.getenv('START_LAT')
-END_LON = os.getenv('END_LON')
-END_LAT = os.getenv('END_LAT')
+START_LON = 12.555 
+START_LAT = 49.67 
+END_LON = 12.61
+END_LAT = 49.801 
 
-INPUT_LOGLEVEL = os.getenv('INPUT_LOGLEVEL',logging.DEBUG)
+#INPUT_LOGLEVEL = DEBUG
 
-process_id = os.getenv('PROCESS_ID')
+process_id = "test_frostdatelatlon"
 
-def setup_logger(logger_name, log_file, level=INPUT_LOGLEVEL):
+#def setup_logger(logger_name, log_file, level=INPUT_LOGLEVEL):
+def setup_logger(logger_name, log_file):
   """Function setup as many loggers as needed"""
 
   l = logging.getLogger(logger_name)
@@ -31,7 +32,8 @@ def setup_logger(logger_name, log_file, level=INPUT_LOGLEVEL):
   streamHandler = logging.StreamHandler()
   streamHandler.setFormatter(formatter)
 
-  l.setLevel(level)
+  l.setLevel(logging.DEBUG)
+  #l.setLevel(level)
   l.addHandler(fileHandler)
   l.addHandler(streamHandler)
 
