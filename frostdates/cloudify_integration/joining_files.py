3910195f1ece29d35b4cd0159552d62e0364d0b8
xeupmara <xeupmara@hawk-login05.ib0.hawk.hww.hlrs.de>
2020-10-02 19:21:04 +0200

    added script for concatenating output geojson files

---
+++ frostdates/cloudify_integration/joining_files.py
@@ -0,0 +1,14 @@
+import glob, os
+filenames =[]
+path=os.path.abspath(__file__ + "/../")
+os.chdir(path+"/export/")
+for file in glob.glob("*.geojson"):
+    filenames.append(file)
+
+filenames.sort()
+
+with open(path+"/export/"+'output_concatenated_files.txt', 'w') as outfile:
+    for fname in filenames:
+        with open(fname) as infile:
+            for line in infile:
+                outfile.write(line)
